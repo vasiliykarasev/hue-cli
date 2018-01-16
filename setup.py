324711e51f78873435bf1b059ca0740e22ea0fa6
vasiliy <karasev00@gmail.com>
2018-01-15 23:35:23 -0800

    Add gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 
 setup(
-    name = "hue executor",
+    name = "hue cli",
     version = "0.0.4",
     author_email = "karasev00@gmail.com",
     description = ("Hue CLI"),
